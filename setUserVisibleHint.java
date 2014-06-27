b946b12195a87d5796802924ec22d5a6f5caa768
kevthanewversi <murimiclerk@gmail.com>
2014-06-27 12:01:52 +0300

    Create setUserVisibleHint.java
    This method is runs automatically each time a fragment is shown.

---
+++ setUserVisibleHint.java
@@ -0,0 +1,10 @@
+//This method is runs automatically each time a fragment is shown
+@Override
+	public void setUserVisibleHint(boolean isVisibleToUser){
+		super.setUserVisibleHint(isVisibleToUser);
+		if(isVisibleToUser){
+	   	//Code to be run whenever fragment becomes visible to the user
+			Log.d(getTag(), "WORKS LIKE A CHARM"); //just to test if it's working
+			}
+		else{}
+	}
